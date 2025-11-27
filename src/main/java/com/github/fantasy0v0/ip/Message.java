de857ee5f67640a4a814d7727dbf771b1cdce4f2
fan <xiaofanms@outlook.com>
2025-11-27 17:16:39 +0800

    Add Helidon.

---
+++ src/main/java/com/github/fantasy0v0/ip/Message.java
@@ -0,0 +1,27 @@
+package com.github.fantasy0v0.ip;
+
+public class Message {
+
+    private String message;
+
+    private String greeting;
+
+    public Message() {
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getGreeting() {
+        return this.greeting;
+    }
+}
---
+++ src/test/java/com/github/fantasy0v0/ip/MainTest.java
@@ -0,0 +1,11 @@
+package com.github.fantasy0v0.ip;
+
+import io.helidon.webserver.testing.junit5.DirectClient;
+import io.helidon.webserver.testing.junit5.RoutingTest;
+
+@RoutingTest
+class MainTest extends AbstractMainTest {
+    MainTest(DirectClient client) {
+        super(client);
+    }
+}
